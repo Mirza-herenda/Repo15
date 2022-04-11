0bd04f6702a6be4295b8c85cd5544ab0895ccf2e
Mirza <herendamirza87@gmail.com>
2022-04-12 00:03:22 +0200

    using args

---
+++ ex08/src/index.js
@@ -0,0 +1,8 @@
+
+const product=(...args) =>
+{ 
+    return args.reduce((a, b) => a * b, 1);
+    }
+    console.log(product(1,2));
+    console.log(product(4,3,10,2));
+    module.exports = product;
