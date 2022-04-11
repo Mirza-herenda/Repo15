4686d93e415ce4086dd0d0575ef4535737b933bf
Mirza <herendamirza87@gmail.com>
2022-04-11 20:56:46 +0200

    anonymous var

---
+++ ex05/src/index.js
@@ -0,0 +1,10 @@
+// Only change code below this line
+const arena =()=> 
+{
+    const arena="new date";
+    "use strict";
+    return new Date();
+
+    };
+    console.log(arena());
+    module.exports = arena;
